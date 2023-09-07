ad7d59aaec0525366e481311892cf8636734940b
Wermutton <manimateus99@gmail.com>
2023-09-06 20:59:32 -0700

    feat: wip example

--- tests/models/test_openai.py
+++ tests/models/test_openai.py
@@ -1,9 +1,14 @@
-# import unittest
+import unittest
+import os
+from dotenv import load_dotenv
 
-# from diversity_measures.models import OpenAIModel
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-# class TestOpenAI(unittest.TestCase):
-#     def test_basic(self):
-#         openai_model = OpenAIModel(openai_api_key="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", model="gpt-3.5-turbo")
-#         response = openai_model.generate("hi", count=1)
-#         print(response)
+from diversity_measures.models import OpenAIModel
+
+class TestOpenAI(unittest.TestCase):
+    def test_basic(self):
+        openai_model = OpenAIModel(openai_api_key=openai_api_key, model="gpt-3.5-turbo")
+        response = openai_model.generate("hi", count=1)
+        print(response)
