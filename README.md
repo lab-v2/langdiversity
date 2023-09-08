84ac245ba99617f21bffc008b2f14c7de108d669
hwelsters <corevalue.noel@gmail.com>
2023-09-08 12:10:31 -0700

    feat: made min the default for selection

---
+++ README.md
@@ -0,0 +1,19 @@
+# Diversity Measures
+
+## Links
+Paper: https://arxiv.org/abs/2308.11189
+
+## 1. Introduction
+
+## 4. Bibtex
+If you used this software in your work please cite our paper
+```
+@misc{ngu2023diversity,
+      title={Diversity Measures: Domain-Independent Proxies for Failure in Language Model Queries}, 
+      author={Noel Ngu and Nathaniel Lee and Paulo Shakarian},
+      year={2023},
+      eprint={2308.11189},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+```
--- diversity_measures/utils/prompt_selection.py
+++ diversity_measures/utils/prompt_selection.py
@@ -3,31 +3,13 @@ from typing import List
 from diversity_measures.measures import AbstractMeasure
 from diversity_measures.models import AbstractBaseModel
 
-# class PromptSelection:
-#     def __init__(self):
-#         pass
-
-#     def find_extreme_scores(self, all_scores, measures, selection_method='max'):
-#         extreme_values = {}
-
-#         for measure in measures:
-#             scores_for_measure = [score[measure] for score in all_scores]
-
-#             if selection_method == 'max':
-#                 extreme_values[measure] = max(scores_for_measure)
-#             elif selection_method == 'min':
-#                 extreme_values[measure] = min(scores_for_measure)
-
-#         return extreme_values
-
-
 class PromptSelection:
     def __init__(
         self,
         model: AbstractBaseModel,
         diversity_measure: AbstractMeasure,
         num_responses: int = 1,
-        selection: str = "max",
+        selection: str = "min",
     ):
         valid_selections = ["min", "max"]
         if selection not in valid_selections:
